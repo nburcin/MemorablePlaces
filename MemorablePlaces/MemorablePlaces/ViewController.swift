36bd700132644bde6a72de46105ad643db255bd4
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-31 17:38:47 -0400

    Initial Setup with Navigation Controller

---
+++ MemorablePlaces/MemorablePlaces/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MemorablePlaces
+//
+//  Created by Nicholas Burcin on 10/31/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(activePlace)
+    }
+
+
+}
+
